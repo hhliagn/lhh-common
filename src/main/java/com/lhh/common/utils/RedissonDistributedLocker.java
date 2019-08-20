503bf1b656881dcc4cea180402d1691f26f84d93
hhliagn <hhliagn@gamil.com>
2019-08-21 01:25:41 +0800

    add part of redis

---
+++ src/main/java/com/lhh/common/utils/RedissonDistributedLocker.java
@@ -0,0 +1,57 @@
+package com.lhh.common.utils;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.xml.ws.soap.Addressing;
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+public class RedissonDistributedLocker implements DistributedLocker {
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    @Override
+    public RLock lock(String lockKey) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock();
+        return lock;
+    }
+
+    @Override
+    public RLock lock(String lockKey, long timeout) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock(timeout, TimeUnit.SECONDS);
+        return lock;
+    }
+
+    @Override
+    public RLock lock(String lockKey, TimeUnit unit, long timeout) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock(timeout, unit);
+        return null;
+    }
+
+    @Override
+    public boolean tryLock(String lockKey, TimeUnit unit, long waitTime, long leaseTime) {
+        RLock lock = redissonClient.getLock(lockKey);
+        try {
+            return lock.tryLock(waitTime,leaseTime,unit);
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void unlock(String lockKey) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.unlock();
+    }
+
+    @Override
+    public void unlock(RLock lock) {
+        lock.unlock();
+    }
+}
